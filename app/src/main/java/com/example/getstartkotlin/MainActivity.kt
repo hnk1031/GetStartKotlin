23f034d4de7e8acc3a4819617559971a09afa9ac
Honoka Atsuchi <>
2019-07-01 22:41:46 +0900

    Initial commit

---
+++ app/src/main/java/com/example/getstartkotlin/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.getstartkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById(R.id.textView) as TextView
+        textView.text = "hogehoge"
+
+
+    }
+}
